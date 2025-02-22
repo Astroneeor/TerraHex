dbf606c03f534931c8d1673411f380325b665d98
Asim424 <asimhamdan2005@gmail.com>
2025-02-22 13:49:28 -0700

    more fun

--- fun.py
+++ fun.py
@@ -4,31 +4,33 @@ def ggpa(unparsed):
         splits an ascii $GPGGALONG into UTC time, latidute and longitude position and direction, 
         and time between measurements
     '''
-    body = unparsed.split(',')
+    data = unparsed.read().splitlines()
     final = []
-    body[1] = int(body[1][0:2]) + float(body[1][2:])/60 
-    body[3] = int(body[3][0:2]) + float(body[3][2:])/60 
-    for i in range(len(body)):
-        if i in [0,1,2,3,4,8]:
-            final.append(body[i])
+    for i in data:        
+        body = i.split(',')
+        line = []
+        body[1] = int(body[1][0:2]) + float(body[1][2:])/60 
+        body[3] = int(body[3][0:2]) + float(body[3][2:])/60 
+        for j in range(len(body)):
+            if j in [0,1,2,3,4,8]:
+                line.append(body[j])
+        final.append(line)
     return final
 
 def best_pos(unparsed):
-    body = unparsed.split(',')
+    data = unparsed.read().splitlines()
     final = []
-    for i in range(len(body)):
-        if i in [1,2,3]:
-            final.append(body[i])
+    for i in data:        
+        body = i.split(',')
+        line = []
+        for j in range(len(body)):
+            if j in [1,2,3]:
+                line.append(body[j])
+        final.append(line)
     return final
 
-def plot():
-    pass
-
-def path_smoothing():
-    pass
 
 if __name__ == "__main__":
     F = open("GGPA.txt",'r')
-    data = F.read().splitlines()
-    for i in data:
-        print(best_pos(i))
+    print(ggpa(F))
+    

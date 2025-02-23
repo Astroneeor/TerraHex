2a3d917e80fecc2bdce0fcaa94c9a489d408f1bb
Neeor Alam <nerroralam@gmail.com>
2025-02-23 08:04:08 -0700

    Updating 3D plot and gifs

--- 3D_mapping_plot.py
+++ 3D_mapping_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter1d
 
 csv = open('output_best.csv', 'r')
 line = csv.read().splitlines()
@@ -22,5 +23,19 @@ ax.set_ylabel('Longitude')
 ax.set_zlabel('Altitude')
 
 plt.savefig("3D_mapping_plot.png", dpi=600, bbox_inches='tight')
+ax.cla()
+
+sigma = 10  # Standard deviation for smoothing
+smoothed_latitude_gauss = gaussian_filter1d(x, sigma=sigma)
+smoothed_longitude_gauss = gaussian_filter1d(y, sigma=sigma)
+smoothed_altitude_gauss = gaussian_filter1d(z, sigma=sigma)
+
+ax.scatter(smoothed_latitude_gauss, smoothed_longitude_gauss, smoothed_altitude_gauss, c = 'r', s = 10)
+ax.set_title('Smoothed Path taken while surveying')
+ax.set_xlabel('Latitude')
+ax.set_ylabel('Longitude')
+ax.set_zlabel('Altitude')
+
+plt.savefig("3D_mapping_plot_smoothed.png", dpi=600, bbox_inches='tight')
 plt.show()
 
